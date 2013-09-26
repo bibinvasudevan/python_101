f9fc554f1b8d456644ed258a3e6ffd20cf9a4ea4
Nanda Kishore <jnandakishore@zeomega.com>
2013-09-26 18:27:30 +0530

    Adding all the stuff required for a HTML5 slideshow

---
+++ slide_config.js
@@ -0,0 +1,35 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Introduction to Python',
+    subtitle: 'High Speed crash course on Python basics.<br/>Hopefully nothing crashes ;-)',
+    //eventTitle: 'Google I/O 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    // favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Nanda Kishore',
+    company: 'Tech Lead, ZeOmega',
+    email: 'madhav.bnk@gmail.com',
+    github: 'http://github.com/none-da'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
